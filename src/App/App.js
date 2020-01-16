a52e634ac82add223e60df5ed488baa971a15829
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-15 19:15:14 -0600

    Nabar with a lil bit of routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConnection from '../helpers/data/connection';
 
 import Auth from '../components/pages/Auth/Auth';
+import Navbar from '../components/shared/Navbar/Navbar';
+import Home from '../components/pages/Home/Home';
 
 import './App.scss';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 firebaseConnection();
 
@@ -33,7 +49,13 @@ class App extends React.Component {
     const { authed } = this.state;
     return (
       <div className="App">
-        <Auth authed={authed} />
+        <Router>
+          <Navbar authed={authed} />
+          <Switch>
+            <PrivateRoute path="/" exact component={Home} authed={authed} />
+            <PublicRoute path="/auth" exact component={Auth} authed={authed} />
+          </Switch>
+        </Router>
       </div>
     );
   }
