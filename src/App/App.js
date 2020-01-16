05e00fcb4254e196a68dcb84d0193bdb347bc88a
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-15 18:26:09 -0600

    completed setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-warning">ADD TO MY COLLECTION</button>
+    </div>
+  );
+}
+
+export default App;
