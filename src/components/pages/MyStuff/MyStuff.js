b4c46c861d82b6f50ab34afeb2c79aa780b536cf
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-15 20:34:55 -0600

    data is under control

--- src/components/pages/MyStuff/MyStuff.js
+++ src/components/pages/MyStuff/MyStuff.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import StuffCard from '../../shared/StuffCard/StuffCard';
+
+import stuffData from '../../../helpers/data/stuffData';
+import authData from '../../../helpers/data/authData';
 
 import './MyStuff.scss';
 
 class MyStuff extends React.Component {
+  state = {
+    stuffs: [],
+  }
+
+  getStuff = () => {
+    stuffData.getStuffByUid(authData.getUid())
+      .then((stuffs) => this.setState({ stuffs }))
+      .catch((err) => console.error('errorfrom get stuff', err));
+  }
+
+  componentDidMount() {
+    this.getStuff();
+  }
+
   render() {
     return (
       <div className="MyStuff">
         <h1>My Stuff</h1>
-        <div className="boards d-flex flex-wrap justify-content-center">
-          <p>This is where my items go</p>
+        <div className="MyStuff d-flex flex-wrap justify-content-center">
+          {this.state.stuffs.map((stuff) => <StuffCard key={stuff.id} stuff={stuff} />)}
         </div>
       </div>
     );
